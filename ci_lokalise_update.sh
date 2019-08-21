8a7b1fb84cee5f7ad3e2ad119cf917a11ed32401
Azamat Kalmurzayev <kalmurzayev@strongte.am>
2019-08-21 15:12:12 +0300

    refactored script file

--- ci_lokalise_update.sh
+++ ci_lokalise_update.sh
@@ -10,23 +10,10 @@
 # - залогиненный github account на локальной машине
 # - сообщить администратору Lokalise проекта свой github account
 ​
-lokaliseTempFolder='.lokalise_temp'
-lokaliseProjectName='AlfaMobile_Front-Localizable'
-​
-lokalise_cli --token $LOKALISE_TOKEN export $LOKALISE_PROJECT_ID --type strings --export_all 1 --export_empty base
-if [ -z $lokaliseProjectName.zip ]
-then
-    echo "No $lokaliseProjectName.zip file found"
-    exit 1
-fi
-echo "MainProject: Updating remotely localization submodule"
-mkdir $lokaliseTempFolder
-unzip -o $lokaliseProjectName.zip -d $lokaliseTempFolder
-​
+echo "Localization: export and unzip files"
+./lokalise_cli --token $LOKALISE_TOKEN export $LOKALISE_PROJECT_ID --type strings --unzip_to . --export_all 1 --export_empty base
 echo "Localization: copying & pushing upstream"
-cp -r $lokaliseTempFolder/* .
-
 git remote set-url origin https://alfawatson:$GH_TOKEN@github.com/alfawatson/alfamobile-localization-ios.git
 git add -u
-git commit -m "[ci skip] update iOS strings from Travis build stages"
+git commit -m "[skip ci] update iOS strings from Travis build stages"
 git push origin HEAD || { echo "Failed to push upstream localization submodule"; exit 1; }
